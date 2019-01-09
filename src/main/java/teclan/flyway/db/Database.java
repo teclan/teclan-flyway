1799376217db0eef109e8666b912c61f0ca28c26
teclan <tbj621@163.com>
2019-01-09 15:36:48 +0800

    修改以支持原生dataSource

--- src/main/java/teclan/flyway/db/Database.java
+++ src/main/java/teclan/flyway/db/Database.java
@@ -67,6 +67,25 @@ public class Database {
         }
     }
 
+
+    public static void initDb(javax.sql.DataSource dataSource) throws FlywayException {
+
+        Flyway flyway = new Flyway();
+        flyway.setDataSource(dataSource);
+        flyway.setBaselineOnMigrate(true);
+        if (migrateClean) {
+            flyway.clean();
+        }
+
+        try {
+            flyway.migrate();
+        } catch (FlywayException e) {
+            LOGGER.error(
+                    "数据库迁移检验失败,表结构可能已经修改,建议迁移之前将 migrateClean 设为 true,如果是生产环境请联系DBA确认后重试 !");
+            throw e;
+        }
+    }
+
 	private static String generateKeyForPool(DataSource dataSource) {
         StringBuffer appender = new StringBuffer();
 
@@ -79,6 +98,7 @@ public class Database {
 
         return Strings.toHexDigest(appender.toString(), "MD5");
     }
+
 
 	private static HikariDataSource generateHikariDataSource(DataSource dataSource) {
 
@@ -117,7 +137,7 @@ public class Database {
      * @param migrateClean
      */
     public void setMigrateClean(boolean migrateClean) {
-        this.migrateClean = migrateClean;
+        Database.migrateClean = migrateClean;
     }
 
 }
